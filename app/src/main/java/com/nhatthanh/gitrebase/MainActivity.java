ff3ff5347392e73f70fa339fb0dab55c05e3c47f
Le Nhat Thanh <thanhdubai9944@gmail.com>
2022-08-19 09:42:58 +0700

    add all

---
+++ app/src/main/java/com/nhatthanh/gitrebase/MainActivity.java
@@ -0,0 +1,17 @@
+package com.nhatthanh.gitrebase;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.nhatthanh.gitrebase.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
